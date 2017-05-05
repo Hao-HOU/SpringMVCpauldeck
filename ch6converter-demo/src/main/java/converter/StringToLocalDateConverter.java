ad6161f09367af830f800a235b86d811cef4fb12
Hao <houhao118@163.com>
2017-05-05 17:10:55 +0800

    增加ch6converter-demo

---
+++ ch6converter-demo/src/main/java/converter/StringToLocalDateConverter.java
@@ -0,0 +1,29 @@
+package converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Created by Hao on 2017/5/5.
+ */
+public class StringToLocalDateConverter implements Converter<String, LocalDate> {
+    private String datePattern;
+
+    public StringToLocalDateConverter(String datePattern){
+        this.datePattern = datePattern;
+    }
+
+    @Override
+    public LocalDate convert(String s){
+        try {
+            return LocalDate.parse(s, DateTimeFormatter.ofPattern(datePattern));
+        } catch (DateTimeParseException e){
+            throw new IllegalArgumentException(
+                    "Invalid date format. Please use this pattern\"" + datePattern + "\""
+            );
+        }
+    }
+}
