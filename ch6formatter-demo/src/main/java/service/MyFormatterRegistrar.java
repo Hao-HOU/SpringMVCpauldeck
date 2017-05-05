a74ef69847f62219d362195d6b2bfb8296999554
Hao <houhao118@163.com>
2017-05-05 21:17:28 +0800

    改为使用Registrar注册Formatter

---
+++ ch6formatter-demo/src/main/java/service/MyFormatterRegistrar.java
@@ -0,0 +1,20 @@
+package service;
+
+import formatter.LocalDateFormatter;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
+
+/**
+ * Created by Hao on 2017/5/5.
+ */
+public class MyFormatterRegistrar implements FormatterRegistrar {
+    private String datePattern;
+    public MyFormatterRegistrar(String datePattern) {
+        this.datePattern = datePattern;
+    }
+
+    @Override
+    public void registerFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter(datePattern));
+    }
+}
