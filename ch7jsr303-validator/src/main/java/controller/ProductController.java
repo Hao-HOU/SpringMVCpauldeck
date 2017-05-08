014c72c6d78c8faf21c81e39f543cb71e608da1c
Hao <houhao118@163.com>
2017-05-08 16:22:33 +0800

    新增ch7jsr303-validator，但是@Past不支持LocalDate类型，编译未通过

---
+++ ch7jsr303-validator/src/main/java/controller/ProductController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import domain.Product;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+/**
+ * Created by Hao on 2017/5/8.
+ */
+@Controller
+public class ProductController {
+    private static final Log logger = LogFactory.getLog(ProductController.class);
+
+    @RequestMapping(value = "/add-product")
+    public String inputProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "ProductForm";
+    }
+
+    @RequestMapping(value = "/save-product")
+    public String saveProduct(@Valid @ModelAttribute Product product, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()){
+            FieldError fieldError = bindingResult.getFieldError();
+            logger.debug("Code: " + fieldError.getCode() + ",object: " + fieldError.getObjectName() + ", field: "
+                    + fieldError.getField());
+            return "ProductForm";
+        }
+
+        model.addAttribute("product", product);
+        return "ProductDetails";
+    }
+}
---
+++ ch7jsr303-validator/src/main/java/domain/Product.java
@@ -0,0 +1,53 @@
+package domain;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * Created by Hao on 2017/5/8.
+ */
+public class Product implements Serializable {
+    private static final long serialVersionUID = -5521544309275815404L;
+    @Size(min = 1, max = 10)
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    @Past
+    private LocalDate productionDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(LocalDate productionDate) {
+        this.productionDate = productionDate;
+    }
+}
