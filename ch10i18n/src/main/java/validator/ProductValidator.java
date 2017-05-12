396c69f94640cfa411a076486cd35e0cffa1f92d
Hao HOU <houhao118@163.com>
2017-05-12 14:21:32 +0800

    新增ch10i18n，目前中文乱码，待解决

---
+++ ch10i18n/src/main/java/validator/ProductValidator.java
@@ -0,0 +1,28 @@
+package validator;
+
+import domain.Product;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by lttxi on 2017/5/12.
+ */
+public class ProductValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> klass) {
+        return Product.class.isAssignableFrom(klass);
+    }
+    @Override
+    public void validate(Object target, Errors errors) {
+        Product product = (Product) target;
+        ValidationUtils.rejectIfEmpty(errors, "name", "productname.required");
+        BigDecimal price = product.getPrice();
+        if (price != null && price.compareTo(BigDecimal.ZERO) < 0) {
+            errors.rejectValue("price", "price.negative");
+        }
+    }
+}
---
+++ ch10i18n/src/main/webapp/WEB-INF/jsp/ProductDetails.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lttxi
+  Date: 2017/5/12
+  Time: 10:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Save Product</title>
+    <style type="text/css">@import url("<c:url value="/css/main.css"/>");</style>
+</head>
+<body>
+<div id="global">
+    <h4>The product has been saved.</h4>
+    <p>
+    <h5>Details:</h5>
+    Product Name: ${product.name}<br/>
+    Description: ${product.description}<br/>
+    Price: $${product.price}
+    </p>
+</div>
+</body>
+</html>
