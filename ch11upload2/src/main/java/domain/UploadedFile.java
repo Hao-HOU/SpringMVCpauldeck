525c7dc3d1a40ece71f0384ed12232dc8f3fe10a
Hao HOU <houhao118@163.com>
2017-05-12 17:16:28 +0800

    新增ch11upload2，路径问题待解决

---
+++ ch11upload2/src/main/java/domain/UploadedFile.java
@@ -0,0 +1,22 @@
+package domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+
+/**
+ * Created by lttxi on 2017/5/12.
+ */
+public class UploadedFile implements Serializable {
+    private static final long serialVersionUID = -783678928610448472L;
+
+    private MultipartFile multipartFile;
+
+    public MultipartFile getMultipartFile() {
+        return multipartFile;
+    }
+
+    public void setMultipartFile(MultipartFile multipartFile) {
+        this.multipartFile = multipartFile;
+    }
+}
